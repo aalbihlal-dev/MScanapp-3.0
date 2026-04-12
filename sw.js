13add4d0508eb9ecbe42eb3fb79fd37c7fd23ccc
aalbihlal-dev <a.albihlal@gmail.com>
2026-04-12 17:02:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='module-scanner-v17';
+var CACHE='module-scanner-v18';
 var URLS=['/','/index.html'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}));self.skipWaiting()});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}));self.clients.claim()});
